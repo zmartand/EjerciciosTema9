cfebd9b7f88a3190e8e06c8e18e393d2bf6b194b
Zara Martín <zmartand@myuax.es>
2023-03-17 16:10:23 +0100

    clase hija Segway y metodos

--- src/main/java/Ejercicio1/Segway.java
+++ src/main/java/Ejercicio1/Segway.java
@@ -1,6 +1,27 @@
 package Ejercicio1;
 
+import java.time.LocalDate;
+
 public class Segway extends Ciclo {
     private int minAltura;
     private int autonomia;
+
+    public Segway(String marca, String modelo, LocalDate fechaFabricacion, int minAltura, int autonomia) {
+        super(marca, modelo, fechaFabricacion);
+        this.minAltura = minAltura;
+        this.autonomia = autonomia;
+    }
+    public int getMinAltura() {
+        return minAltura;
+    }
+    public int getAutonomia() {
+        return autonomia;
+    }
+    public double getAlquilerHora() {
+        return 18.90;
+    }
+    public String toString(){
+        return String.format("- Segway %s %d cm %d km %s%.2f/hora",
+                super.toString(), getMinAltura(), getAutonomia(), "€", getAlquilerHora());
+    }
 }
