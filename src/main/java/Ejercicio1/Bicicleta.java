99640cc582478b19ff3d169321d47fe4bc968128
Zara Martín <zmartand@myuax.es>
2023-03-17 16:08:51 +0100

    clase hija Bicicleta y metodos

--- src/main/java/Ejercicio1/Bicicleta.java
+++ src/main/java/Ejercicio1/Bicicleta.java
@@ -1,6 +1,22 @@
 package Ejercicio1;
 
+import java.time.LocalDate;
+
 public class Bicicleta extends Ciclo{
     private int numCambios;
+    public Bicicleta(String marca, String modelo, LocalDate fechaFabricacion, int numCambios) {
+        super(marca, modelo,fechaFabricacion);
+        this.numCambios = numCambios;
+    }
+    public int getNumCambios() {
+        return numCambios;
+    }
+    public double getAlquilerHora() {
+        return 4.90;
+    }
+    public String toString(){
+        return String.format("- Bici %s %d velocidades %s%.2f/hora",
+                super.toString(), getNumCambios(), "€", getAlquilerHora());
+    }
 
 }
