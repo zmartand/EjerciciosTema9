11120dd259c779ce0e316ff0677efe886c19422c
Zara Martín <zmartand@myuax.es>
2023-03-17 16:13:28 +0100

    clase hija Giroscopio y metodos

--- src/main/java/Ejercicio1/Giroscopio.java
+++ src/main/java/Ejercicio1/Giroscopio.java
@@ -1,5 +1,24 @@
 package Ejercicio1;
 
-public class Giroscopio {
+import java.time.LocalDate;
+
+public class Giroscopio extends Ciclo {
     private int autonomia;
+
+    public Giroscopio(String marca, String modelo, LocalDate fechaFabricacion, int autonomia) {
+        super(marca, modelo, fechaFabricacion);
+        this.autonomia = autonomia;
+    }
+    public int getAutonomia() {
+        return autonomia;
+    }
+    public double getAlquilerHora() {
+        return 29.90;
+    }
+    public String toString(){
+        int horas = getAutonomia() / 60;
+        int minutos = getAutonomia() % 60;
+        return String.format("- Giroscopio %s %d km de autonomía %s%.2f/hora",
+                super.toString(), getAutonomia(), "€", getAlquilerHora());
+    }
 }
