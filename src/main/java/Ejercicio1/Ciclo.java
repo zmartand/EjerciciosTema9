dbe711327302862c8a58e5f6b03f51515e927f30
Zara Martín <zmartand@myuax.es>
2023-03-17 16:06:03 +0100

    clase abstracta y métodos

--- src/main/java/Ejercicio1/Ciclo.java
+++ src/main/java/Ejercicio1/Ciclo.java
@@ -6,5 +6,24 @@ public abstract class Ciclo {
     private String marca;
     private String modelo;
     private LocalDate fechaFabricacion;
+    public Ciclo(String marca, String modelo, LocalDate fechaFabricacion) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.fechaFabricacion = fechaFabricacion;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public LocalDate getFechaFabricacion() {
+        return fechaFabricacion;
+    }
+    public abstract double getAlquilerHora();
+    public String toString(){
+        return String.format("%s %s (%d años)", getMarca(), getModelo(),
+                LocalDate.now().getYear() - getFechaFabricacion().getYear());
+    }
 
 }
