0ed044d939765b42f555255ada1b1d3d29ae348e
Zara Martín <zmartand@myuax.es>
2023-03-18 20:57:54 +0100

    interfaz lista

--- src/main/java/Ejercicio2/Traduccion.java
+++ src/main/java/Ejercicio2/Traduccion.java
@@ -1,4 +1,9 @@
 package Ejercicio2;
 
 public interface Traduccion {
+    String introducirDistancia();
+    String introducirTiempo();
+    void iniciarRespuesta();
+    void finalizarRespuesta();
+    void mostrarVelocidad(double velocidad);
 }
